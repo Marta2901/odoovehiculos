1f62b1c654f0606307b2872790dda09287407927
Marta2901 <martaenriquezfigueroa@gmail.com>
2026-01-19 08:30:03 +0100

    Editado manifest y vehiculo.py de la carpeta models

--- models/vehiculo.py
+++ models/vehiculo.py
@@ -1,11 +1,11 @@
 # odoovehiculos/models/vehiculo.py
 from odoo import models, fields
+    
+class VehiculoManagement(models.Model):
+    _name = 'vehiculo.management'
+    _description = 'Gestión de vehículos'
 
-    class vehiculo(models.Model):
-        _name = 'vehiculo.management'
-        _description = 'Gestión de vehículos'
-
-        matricula= fields.Char(string='Matrícula', required=True)
-        marca = fields.Char(string='Marca', required=True)
-        modelo = fields.Char(string='Modelo', required=True)
-        activo = fields.Boolean(string='¿Activo?', default=True)
+    matricula= fields.Char(string='Matrícula', required=True)
+    marca = fields.Char(string='Marca', required=True)
+    modelo = fields.Char(string='Modelo', required=True)
+    activo = fields.Boolean(string='¿Activo?', default=True)
